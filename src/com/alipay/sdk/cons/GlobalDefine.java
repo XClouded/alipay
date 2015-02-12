cbb2dcc3c91b7096e2239ff4f706e5f70512a4be
jiayouxjh <xjhssg@gmail.com>
2015-02-12 14:59:06 +0800

    修改cons目录下的代码格式

--- src/com/alipay/sdk/cons/GlobalDefine.java
+++ src/com/alipay/sdk/cons/GlobalDefine.java
@@ -2,23 +2,18 @@ package com.alipay.sdk.cons;
 
 public class GlobalDefine
 {
-  public static final String a = "https";
-  public static final String b = "user_agent";
-  public static final String c = "tid";
-  public static final String d = "external_info";
-  public static final String e = "has_alipay";
-  public static final String f = "has_msp_app";
-  public static final String g = "result";
-  public static final String h = "memo";
-  public static final String i = "resultStatus";
-  public static final String j = "call_back_url";
-  public static final String k = "utdid";
-  public static final String l = "app_key";
-  public static final String m = "trideskey";
-  public static final String n = "new_client_key";
+    public static final String a = "https";
+    public static final String b = "user_agent";
+    public static final String c = "tid";
+    public static final String d = "external_info";
+    public static final String e = "has_alipay";
+    public static final String f = "has_msp_app";
+    public static final String g = "result";
+    public static final String h = "memo";
+    public static final String i = "resultStatus";
+    public static final String j = "call_back_url";
+    public static final String k = "utdid";
+    public static final String l = "app_key";
+    public static final String m = "trideskey";
+    public static final String n = "new_client_key";
 }
-
-/* Location:           /Users/xujh/Downloads/WS_MOBILE_PAY_SDK_BASE/移动支付接口SDK2.0标准版(20150121)/DEMO/客户端demo/支付宝移动支付SDK标准版(Android 2.2)/alipay-sdk-common/alipaysdk.jar
- * Qualified Name:     com.alipay.sdk.cons.GlobalDefine
- * JD-Core Version:    0.6.2
- */
--- src/com/alipay/sdk/cons/MiniDefine.java
+++ src/com/alipay/sdk/cons/MiniDefine.java
@@ -2,27 +2,22 @@ package com.alipay.sdk.cons;
 
 public class MiniDefine
 {
-  public static final String a = "value";
-  public static final String b = "status";
-  public static final String c = "msg";
-  public static final String d = "form";
-  public static final String e = "onload";
-  public static final String f = "action";
-  public static final String g = "name";
-  public static final String h = "host";
-  public static final String i = "params";
-  public static final String j = "enctype";
-  public static final String k = "request_param";
-  public static final String l = "validate";
-  public static final String m = "https";
-  public static final String n = "formSubmit";
-  public static final String o = "namespace";
-  public static final String p = "apiVersion";
-  public static final String q = "apiName";
-  public static final String r = "tid";
+    public static final String a = "value";
+    public static final String b = "status";
+    public static final String c = "msg";
+    public static final String d = "form";
+    public static final String e = "onload";
+    public static final String f = "action";
+    public static final String g = "name";
+    public static final String h = "host";
+    public static final String i = "params";
+    public static final String j = "enctype";
+    public static final String k = "request_param";
+    public static final String l = "validate";
+    public static final String m = "https";
+    public static final String n = "formSubmit";
+    public static final String o = "namespace";
+    public static final String p = "apiVersion";
+    public static final String q = "apiName";
+    public static final String r = "tid";
 }
-
-/* Location:           /Users/xujh/Downloads/WS_MOBILE_PAY_SDK_BASE/移动支付接口SDK2.0标准版(20150121)/DEMO/客户端demo/支付宝移动支付SDK标准版(Android 2.2)/alipay-sdk-common/alipaysdk.jar
- * Qualified Name:     com.alipay.sdk.cons.MiniDefine
- * JD-Core Version:    0.6.2
- */
