4ff2381a63fcc29c850e2c9c8d3162c83c549200
jiayouxjh <xjhssg@gmail.com>
2015-02-13 15:18:45 +0800

    修改exception目录下代码格式

--- src/com/alipay/sdk/exception/NetErrorException.java
+++ src/com/alipay/sdk/exception/NetErrorException.java
@@ -1,61 +1,54 @@
-/*    */ package com.alipay.sdk.exception;
-/*    */ 
-/*    */ import android.text.TextUtils;
-/*    */ import android.util.Log;
-/*    */ 
-/*    */ public final class NetErrorException extends Exception
-/*    */ {
-/*    */   private static final long serialVersionUID = 8374198311711795611L;
-/*    */   public static final int NET_CONNECTION_ERROR = 0;
-/*    */   public static final int SERVER_ERROR = 1;
-/*    */   public static final int SSL_ERROR = 2;
-/*    */   private int errorCode;
-/*    */ 
-/*    */   public NetErrorException()
-/*    */   {
-/* 31 */     this(null, null);
-/*    */   }
-/*    */ 
-/*    */   public NetErrorException(String paramString) {
-/* 35 */     this(paramString, null);
-/*    */   }
-/*    */ 
-/*    */   public NetErrorException(Throwable paramThrowable) {
-/* 39 */     this(null, paramThrowable);
-/*    */   }
-/*    */ 
-/*    */   public NetErrorException(String paramString, Throwable paramThrowable) {
-/* 43 */     super(paramString, paramThrowable);
-/* 44 */     this.errorCode = 0;
-/* 45 */     printException(paramString, paramThrowable);
-/*    */   }
-/*    */ 
-/*    */   public final int getErrorCode() {
-/* 49 */     return this.errorCode;
-/*    */   }
-/*    */ 
-/*    */   public final void setErrorCode(int paramInt) {
-/* 53 */     this.errorCode = paramInt;
-/*    */   }
-/*    */ 
-/*    */   public static void printException(String paramString, Throwable paramThrowable) {
-/* 57 */     if (!TextUtils.isEmpty(paramString))
-/* 58 */       Log.w("NetError", "NetError--" + paramString);
-/*    */     try
-/*    */     {
-/* 61 */       if (paramThrowable != null) {
-/* 62 */         Log.w("NetError", "NetError--" + paramThrowable.getMessage());
-/*    */       }
-/*    */ 
-/* 65 */       return;
-/*    */     }
-/*    */     catch (Exception localException)
-/*    */     {
-/*    */     }
-/*    */   }
-/*    */ }
+package com.alipay.sdk.exception;
 
-/* Location:           /Users/xujh/Downloads/WS_MOBILE_PAY_SDK_BASE/移动支付接口SDK2.0标准版(20150121)/DEMO/客户端demo/支付宝移动支付SDK标准版(Android 2.2)/alipay-sdk-common/alipaysdk.jar
- * Qualified Name:     com.alipay.sdk.exception.NetErrorException
- * JD-Core Version:    0.6.2
- */
+import android.text.TextUtils;
+import android.util.Log;
+
+public final class NetErrorException extends Exception
+{
+    private static final long serialVersionUID = 8374198311711795611L;
+    public static final int NET_CONNECTION_ERROR = 0;
+    public static final int SERVER_ERROR = 1;
+    public static final int SSL_ERROR = 2;
+    private int errorCode;
+
+    public NetErrorException() {
+        this(null, null);
+    }
+
+    public NetErrorException(String paramString) {
+        this(paramString, null);
+    }
+
+    public NetErrorException(Throwable paramThrowable) {
+        this(null, paramThrowable);
+    }
+
+    public NetErrorException(String paramString, Throwable paramThrowable) {
+        super(paramString, paramThrowable);
+        this.errorCode = 0;
+        printException(paramString, paramThrowable);
+    }
+
+    public final int getErrorCode() {
+        return this.errorCode;
+    }
+
+    public final void setErrorCode(int paramInt) {
+        this.errorCode = paramInt;
+    }
+
+    public static void printException(String paramString, Throwable paramThrowable) {
+        if (!TextUtils.isEmpty(paramString))
+            Log.w("NetError", "NetError--" + paramString);
+        try {
+            if (paramThrowable != null) {
+                Log.w("NetError", "NetError--" + paramThrowable.getMessage());
+            }
+
+            return;
+        } catch (Exception localException) {
+
+        }
+    }
+
+}
--- src/com/alipay/sdk/exception/UnZipException.java
+++ src/com/alipay/sdk/exception/UnZipException.java
@@ -1,49 +1,41 @@
-/*    */ package com.alipay.sdk.exception;
-/*    */ 
-/*    */ import android.text.TextUtils;
-/*    */ import android.util.Log;
-/*    */ 
-/*    */ public final class UnZipException extends Exception
-/*    */ {
-/*    */   private static final long serialVersionUID = 7405333891987087563L;
-/*    */ 
-/*    */   public UnZipException()
-/*    */   {
-/* 25 */     this(null, null);
-/*    */   }
-/*    */ 
-/*    */   public UnZipException(String paramString) {
-/* 29 */     this(paramString, null);
-/*    */   }
-/*    */ 
-/*    */   public UnZipException(Throwable paramThrowable) {
-/* 33 */     this(null, paramThrowable);
-/*    */   }
-/*    */ 
-/*    */   public UnZipException(String paramString, Throwable paramThrowable) {
-/* 37 */     super(paramString, paramThrowable);
-/* 38 */     printException(paramString, paramThrowable);
-/*    */   }
-/*    */ 
-/*    */   public static void printException(String paramString, Throwable paramThrowable) {
-/* 42 */     if (!TextUtils.isEmpty(paramString))
-/* 43 */       Log.e("Validation", "Validation--" + paramString);
-/*    */     try
-/*    */     {
-/* 46 */       if (paramThrowable != null) {
-/* 47 */         Log.e("Validation", "Validation--" + paramThrowable.getMessage());
-/* 48 */         paramThrowable.printStackTrace();
-/*    */       }
-/*    */ 
-/* 51 */       return;
-/*    */     }
-/*    */     catch (Exception localException)
-/*    */     {
-/*    */     }
-/*    */   }
-/*    */ }
+package com.alipay.sdk.exception;
 
-/* Location:           /Users/xujh/Downloads/WS_MOBILE_PAY_SDK_BASE/移动支付接口SDK2.0标准版(20150121)/DEMO/客户端demo/支付宝移动支付SDK标准版(Android 2.2)/alipay-sdk-common/alipaysdk.jar
- * Qualified Name:     com.alipay.sdk.exception.UnZipException
- * JD-Core Version:    0.6.2
- */
+import android.text.TextUtils;
+import android.util.Log;
+
+public final class UnZipException extends Exception
+{
+    private static final long serialVersionUID = 7405333891987087563L;
+
+    public UnZipException() {
+        this(null, null);
+    }
+
+    public UnZipException(String paramString) {
+        this(paramString, null);
+    }
+
+    public UnZipException(Throwable paramThrowable) {
+        this(null, paramThrowable);
+    }
+
+    public UnZipException(String paramString, Throwable paramThrowable) {
+        super(paramString, paramThrowable);
+        printException(paramString, paramThrowable);
+    }
+
+    public static void printException(String paramString, Throwable paramThrowable) {
+        if (!TextUtils.isEmpty(paramString))
+            Log.e("Validation", "Validation--" + paramString);
+        try {
+            if (paramThrowable != null) {
+                Log.e("Validation", "Validation--" + paramThrowable.getMessage());
+                paramThrowable.printStackTrace();
+            }
+
+            return;
+        } catch (Exception localException) {
+
+        }
+    }
+}
